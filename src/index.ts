c921b21fba46115c86e70cb88a8d7926d7100f22
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2025-05-06 09:13:21 +0200

    feat: tsup build

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,30 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { OpenAPIParser } from "./openapi/parser.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 
 export const buildMcpServer = async (resourceLocator: string) => {
-    const openApiParser = await OpenAPIParser.from(resourceLocator);
+	const openApiParser = await OpenAPIParser.from(resourceLocator);
 
-    const server = new McpServer(openApiParser.getInfo())
-}
+	const server = new McpServer(openApiParser.getInfo());
+
+	for (const path of openApiParser.getPaths()) {
+		server.tool(
+			path.name,
+			{ parameters: path.parameters, request: path.request },
+			(received) => {
+				console.log('received', received);
+				return {
+					content: [],
+				};
+			},
+		);
+	}
+
+	return server;
+};
+
+const mcpServer = await buildMcpServer("https://petstore3.swagger.io/api/v3/openapi.json");
+
+const transport = new StdioServerTransport()
+
+await mcpServer.connect(transport);
