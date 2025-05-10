15905809b8e34fcebd8d071242926c4c596605c3
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2025-05-10 18:57:51 +0200

    chore: schema createFromParameter

--- tests/openapi/schema.test.ts
+++ tests/openapi/schema.test.ts
@@ -3,35 +3,37 @@ import { createFromParameter, createFromParameters } from "../../src/schema.js";
 import { loadFixtures } from "../utils.js";
 
 describe("schema", () => {
-	const fieldsToGenerate = [
-		"integer",
-		"string",
-		"boolean",
-		"number",
-		"array",
-		"object",
-		"integer not required",
-		"string with enum",
-		"number with min and max",
-		"integer with min and max",
-		"string with minLength and maxLength",
-		"number with default",
-		"unknown type",
-	].map((type, index) => ({ type, index }));
+	describe('createFromParameter', () => {
+		const fieldsToGenerate = [
+			"integer",
+			"string",
+			"boolean",
+			"number",
+			"array",
+			"object",
+			"integer not required",
+			"string with enum",
+			"number with min and max",
+			"integer with min and max",
+			"string with minLength and maxLength",
+			"number with default",
+			"unknown type",
+		].map((type, index) => ({ type, index }));
 
-	it.each(fieldsToGenerate)(
-		"should create a zod schema for a $type type",
-		async ({ index }) => {
-			const petSchema = await loadFixtures("schema.json");
+		it.each(fieldsToGenerate)(
+			"should create a zod schema for a $type type",
+			async ({ index }) => {
+				const petSchema = await loadFixtures("schema.json");
 
-			const petIdParameter =
-				petSchema.paths["/pet/{petId}"].post.parameters[index];
+				const petIdParameter =
+					petSchema.paths["/pet/{petId}"].post.parameters[index];
 
-			const zodSchema = createFromParameter(petIdParameter);
+				const zodSchema = createFromParameter(petIdParameter);
 
-			expect(zodSchema).toMatchSnapshot();
-		},
-	);
+				expect(zodSchema).toMatchSnapshot();
+			},
+		);
+	})
 
 	it('should create a zod schema from an array of parameters', async () => {
 		const petSchema = await loadFixtures("schema.json");
