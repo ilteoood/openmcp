3828576da24d2d972db425adc83c7fc8caa8ef04
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2025-05-10 19:20:10 +0200

    feat: test basic methods

--- tests/openapi/parser.test.ts
+++ tests/openapi/parser.test.ts
@@ -3,14 +3,53 @@ import { OpenAPIParser } from "../../src/openapi/parser.js";
 import { buildFixturePath } from "../utils.js";
 
 describe("parser", () => {
-	it("can correctly return the informations", async () => {
-		const schemaPath = buildFixturePath("schema.json");
+	describe("getInfo", () => {
+		it("can correctly parse a swagger 2.0 schema", async () => {
+			const schemaPath = buildFixturePath("petstore2.json");
 
-		const openApiParser = await OpenAPIParser.from(schemaPath);
+			const openApiParser = await OpenAPIParser.from(schemaPath);
 
-		expect(openApiParser.getInfo()).toStrictEqual({
-			name: "Swagger Petstore - OpenAPI 3.0",
-			version: "1.0.26",
+			expect(openApiParser.getInfo()).toStrictEqual({
+				name: "Swagger Petstore",
+				version: "1.0.7",
+			});
+		});
+
+		it("can correctly parse an openapi 3.0 schema", async () => {
+			const schemaPath = buildFixturePath("petstore3.json");
+
+			const openApiParser = await OpenAPIParser.from(schemaPath);
+
+			expect(openApiParser.getInfo()).toStrictEqual({
+				name: "Swagger Petstore - OpenAPI 3.0",
+				version: "1.0.26",
+			});
+		});
+	});
+
+	describe("getPaths", () => {
+		it("can correctly parse a swagger 2.0 schema", async () => {
+			const schemaPath = buildFixturePath("petstore2.json");
+
+			const openApiParser = await OpenAPIParser.from(schemaPath);
+
+			expect(openApiParser.getPaths()).toMatchSnapshot();
+		});
+
+		it("can correctly parse an openapi 3.0 schema", async () => {
+			const schemaPath = buildFixturePath("petstore3.json");
+
+			const openApiParser = await OpenAPIParser.from(schemaPath);
+
+			expect(openApiParser.getPaths()).toMatchSnapshot();
+		});
+
+		it("can correctly parse a custom schema", async () => {
+			const schemaPath = buildFixturePath("schema.json");
+
+			const openApiParser = await OpenAPIParser.from(schemaPath);
+
+			expect(openApiParser.getPaths()).toMatchSnapshot();
 		});
 	});
 });
