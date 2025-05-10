768f891be6d3c1d1d40a6a4e7dcd4972833036e0
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2025-05-10 18:57:02 +0200

    chore: basePathItemExtractor tests

--- tests/openapi/extractors.test.ts
+++ tests/openapi/extractors.test.ts
@@ -4,27 +4,29 @@ import { basePathItemExtractor } from "../../src/openapi/extractors.js";
 import { loadFixtures } from "../utils.js";
 
 describe("extractors", () => {
-	it("can correctly return base path item", async () => {
-		const openApiDocument = await loadFixtures("schema.json");
+	describe('basePathItemExtractor', () => {
+		it("can correctly return base path item", async () => {
+			const openApiDocument = await loadFixtures("schema.json");
 
-		const result = basePathItemExtractor(
-			"/pet/{petId}",
-			openApiDocument.paths["/pet/{petId}"],
-			OpenAPIV3.HttpMethods.POST,
-		);
+			const result = basePathItemExtractor(
+				"/pet/{petId}",
+				openApiDocument.paths["/pet/{petId}"],
+				OpenAPIV3.HttpMethods.POST,
+			);
 
-		expect(result).toMatchSnapshot();
-	});
+			expect(result).toMatchSnapshot();
+		});
 
-	it("can correctly return base path item - generated name", async () => {
-		const openApiDocument = await loadFixtures("schema.json");
+		it("can correctly return base path item - generated name", async () => {
+			const openApiDocument = await loadFixtures("schema.json");
 
-		const result = basePathItemExtractor(
-			"/pets/location",
-			openApiDocument.paths["/pets/location"],
-			OpenAPIV3.HttpMethods.POST,
-		);
+			const result = basePathItemExtractor(
+				"/pets/location",
+				openApiDocument.paths["/pets/location"],
+				OpenAPIV3.HttpMethods.POST,
+			);
 
-		expect(result).toMatchSnapshot();
-	});
+			expect(result).toMatchSnapshot();
+		});
+	})
 });
